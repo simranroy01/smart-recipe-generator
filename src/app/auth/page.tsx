7ebf685c5241f49bead6c697a243dbb185773612
simranroy01 <simranroy.186@gmail.com>
2025-10-13 19:01:09 +0530

    committed

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,6 +1,8 @@
 // src/app/auth/page.tsx
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { createClient } from '@/lib/supabaseClient'
